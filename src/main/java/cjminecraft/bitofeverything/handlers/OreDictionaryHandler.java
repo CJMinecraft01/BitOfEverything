0d84999960e6673bf9bc51b6c2006d4460370a89
CJMinecraft01 <speed2pc2@gmail.com>
2016-11-28 19:33:55 +0000

    OreDictionary Tutorial

---
+++ src/main/java/cjminecraft/bitofeverything/handlers/OreDictionaryHandler.java
@@ -0,0 +1,17 @@
+package cjminecraft.bitofeverything.handlers;
+
+import cjminecraft.bitofeverything.init.ModBlocks;
+import cjminecraft.bitofeverything.init.ModItems;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
+
+public class OreDictionaryHandler {
+	
+	public static void registerOreDictionary() {
+		OreDictionary.registerOre("oreTin", ModBlocks.tin_ore);
+		OreDictionary.registerOre("ingotTin", ModItems.tinIngot);
+		OreDictionary.registerOre("chipBasic", new ItemStack(ModItems.chip, 1, 0));
+		OreDictionary.registerOre("chipAdvanced", new ItemStack(ModItems.chip, 1, 1));
+	}
+
+}
