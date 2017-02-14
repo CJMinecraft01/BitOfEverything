af264455ef686b70204154e158dcc98df43c091e
CJMinecraft01 <speed2pc2@gmail.com>
2017-02-14 18:40:31 +0000

    Custom Button Tutorial

--- src/main/java/cjminecraft/bitofeverything/blocks/BlockTinButton.java
+++ src/main/java/cjminecraft/bitofeverything/blocks/BlockTinButton.java
@@ -9,26 +9,44 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+/**
+ * A block which is like a button
+ * @author CJMinecraft
+ *
+ */
 public class BlockTinButton extends BlockButton {
 
+	/**
+	 * Default constructor
+	 * @param unlocalizedName The unlocalized name of the block
+	 */
 	public BlockTinButton(String unlocalizedName) {
-		super(false);
+		super(false); //Says that the block is not a wooden button
 		this.setUnlocalizedName(unlocalizedName);
 		this.setRegistryName(new ResourceLocation(Reference.MODID, unlocalizedName));
 		this.setHardness(3);
 		this.setResistance(20);
 	}
 
+	/**
+	 * Plays our custom sound when you press it
+	 */
 	@Override
 	protected void playClickSound(EntityPlayer player, World worldIn, BlockPos pos) {
 		worldIn.playSound(player, pos, BoeSoundHandler.TIN_BUTTON_CLICK_ON, SoundCategory.BLOCKS, 1.0F, 1.0F);
 	}
 
+	/**
+	 * Plays our custom sound when you press it
+	 */
 	@Override
 	protected void playReleaseSound(World worldIn, BlockPos pos) {
 		worldIn.playSound((EntityPlayer)null, pos, BoeSoundHandler.TIN_BUTTON_CLICK_OFF, SoundCategory.BLOCKS, 1.0F, 1.0F);
 	}
 	
+	/**
+	 * How long the button emits a redstone signal
+	 */
 	@Override
 	public int tickRate(World worldIn) {
 		return 15;
