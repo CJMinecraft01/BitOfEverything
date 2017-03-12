4cc850b5deee85aba8be5ba1422ff6c2e863bee8
CJMinecraft01 <speed2pc2@gmail.com>
2017-03-12 12:52:19 +0000

    Fix door dropping wooden door when broken in survival

--- src/main/java/cjminecraft/bitofeverything/blocks/BlockTinDoor.java
+++ src/main/java/cjminecraft/bitofeverything/blocks/BlockTinDoor.java
@@ -1,5 +1,7 @@
 package cjminecraft.bitofeverything.blocks;
 
+import java.util.Random;
+
 import cjminecraft.bitofeverything.Reference;
 import cjminecraft.bitofeverything.handlers.BoeSoundHandler;
 import cjminecraft.bitofeverything.init.ModBlocks;
@@ -8,12 +10,14 @@ import net.minecraft.block.BlockDoor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 
 public class BlockTinDoor extends BlockDoor {
@@ -120,6 +124,17 @@ public class BlockTinDoor extends BlockDoor {
 	@Override
 	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
 		return new ItemStack(ModBlocks.tinDoor);
+	}
+	
+	@Override
+	public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos,
+			EntityPlayer player) {
+		return new ItemStack(ModBlocks.tinDoor);
+	}
+	
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return Item.getItemFromBlock(ModBlocks.tinDoor);
 	}
 
 }
