f20cc5ec16317e2b163c7234cf897574e1f772e8
CJMinecraft01 <speed2pc2@gmail.com>
2017-05-02 17:48:20 +0100

    Update cotton texture. Thanks EmotionFox!

--- src/main/java/cjminecraft/bitofeverything/blocks/BlockCotton.java
+++ src/main/java/cjminecraft/bitofeverything/blocks/BlockCotton.java
@@ -3,21 +3,37 @@ package cjminecraft.bitofeverything.blocks;
 import cjminecraft.bitofeverything.Reference;
 import cjminecraft.bitofeverything.init.ModItems;
 import net.minecraft.block.BlockCrops;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 
+/**
+ * A crop which will craft to make fabric
+ * @author CJMinecraft
+ *
+ */
 public class BlockCotton extends BlockCrops {
 	
+	/**
+	 * Initialize the crop
+	 * @param unloclaizedName The unlocalized name of the block
+	 */
 	public BlockCotton(String unloclaizedName) {
 		this.setUnlocalizedName(unloclaizedName);
 		this.setRegistryName(new ResourceLocation(Reference.MODID, unloclaizedName));
 	}
 	
+	/**
+	 * Get the seed. e.g {@link Items#WHEAT_SEEDS}
+	 */
 	@Override
 	protected Item getSeed() {
 		return ModItems.cotton;
 	}
 	
+	/**
+	 * Get the crop. e.g. {@link Items#WHEAT}
+	 */
 	@Override
 	protected Item getCrop() {
 		return ModItems.cotton;
