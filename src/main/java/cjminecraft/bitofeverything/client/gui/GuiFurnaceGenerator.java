458860ec1ba2767028cb3f2080e8d9061ec7448e
CJMinecraft01 <speed2pc2@gmail.com>
2017-07-23 13:21:48 +0100

    Forge Energy - Energy Producer Tutorial

--- src/main/java/cjminecraft/bitofeverything/client/gui/GuiFurnaceGenerator.java
+++ src/main/java/cjminecraft/bitofeverything/client/gui/GuiFurnaceGenerator.java
@@ -1,15 +1,24 @@
 package cjminecraft.bitofeverything.client.gui;
 
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.Arrays;
+
 import cjminecraft.bitofeverything.Reference;
 import cjminecraft.bitofeverything.client.gui.ProgressBar.ProgressBarDirection;
 import cjminecraft.bitofeverything.container.ContainerFurnaceGenerator;
 import cjminecraft.bitofeverything.network.PacketGetWorker;
 import cjminecraft.bitofeverything.network.PacketHandler;
 import cjminecraft.bitofeverything.tileentity.TileEntityFurnaceGenerator;
+import cjminecraft.core.client.gui.EnergyBar;
+import cjminecraft.core.config.CJCoreConfig;
+import cjminecraft.core.energy.EnergyUtils;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 
 public class GuiFurnaceGenerator extends GuiContainer {
@@ -24,6 +33,7 @@ public class GuiFurnaceGenerator extends GuiContainer {
 	public static int sync = 0;
 	
 	private ProgressBar progressBar;
+	private EnergyBar energyBar;
 	
 	public GuiFurnaceGenerator(IInventory playerInv, TileEntityFurnaceGenerator te) {
 		super(new ContainerFurnaceGenerator(playerInv, te));
@@ -40,6 +50,15 @@ public class GuiFurnaceGenerator extends GuiContainer {
 	@Override
 	public void initGui() {
 		super.initGui();
+		this.energyBar = new EnergyBar(0, (this.width / 2 - this.xSize / 2) + 7, (this.height / 2 - this.ySize / 2) + 16, 18, 54, 0, 0);
+		this.buttonList.add(energyBar);
+	}
+	
+	@Override
+	protected void actionPerformed(GuiButton button) throws IOException {
+		if(button == this.energyBar)
+			this.energyBar.actionPerformed(this.mc, this.te);
+		super.actionPerformed(button);
 	}
 	
 	@Override
@@ -62,7 +81,30 @@ public class GuiFurnaceGenerator extends GuiContainer {
 		sync++;
 		sync %= 10;
 		if(sync == 0) {
+			this.energyBar.syncData(this.te.getPos(), EnumFacing.NORTH);
 			PacketHandler.INSTANCE.sendToServer(new PacketGetWorker(this.te.getPos(), this.mc.player.getAdjustedHorizontalFacing(), "cjminecraft.bitofeverything.client.gui.GuiFurnaceGenerator", "cooldown", "maxCooldown"));
+		}
+		
+		if (this.energyBar.isMouseOver()) {
+			if (CJCoreConfig.MULTIMETER_SIMPLIFY_ENERGY) {
+				this.drawHoveringText(
+						Arrays.asList(
+								EnergyUtils.getEnergyAsString(this.energyBar.energy, CJCoreConfig.DEFAULT_ENERGY_UNIT)
+										+ (CJCoreConfig.MULTIMETER_SHOW_CAPACITY
+												? " / " + EnergyUtils.getEnergyAsString(this.energyBar.capacity,
+														CJCoreConfig.DEFAULT_ENERGY_UNIT)
+												: "")),
+						mouseX - ((this.width - this.xSize) / 2), mouseY - ((this.height - this.ySize) / 2));
+			} else {
+				this.drawHoveringText(Arrays.asList(
+						NumberFormat.getInstance().format(this.energyBar.energy) + " "
+								+ CJCoreConfig.DEFAULT_ENERGY_UNIT.getSuffix()
+								+ (CJCoreConfig.MULTIMETER_SHOW_CAPACITY
+										? " / " + NumberFormat.getInstance().format(this.energyBar.capacity) + " "
+												+ CJCoreConfig.DEFAULT_ENERGY_UNIT.getSuffix()
+										: "")),
+						mouseX - ((this.width - this.xSize) / 2), mouseY - ((this.height - this.ySize) / 2));
+			}
 		}
 	}
 
--- src/main/java/cjminecraft/bitofeverything/tileentity/TileEntityFurnaceGenerator.java
+++ src/main/java/cjminecraft/bitofeverything/tileentity/TileEntityFurnaceGenerator.java
@@ -1,16 +1,28 @@
 package cjminecraft.bitofeverything.tileentity;
 
+import cjminecraft.bitofeverything.blocks.BlockFurnaceGenerator;
+import cjminecraft.bitofeverything.blocks.BlockMachine;
 import cjminecraft.bitofeverything.capabilties.Worker;
 import cjminecraft.bitofeverything.handlers.EnumHandler.ChipTypes;
 import cjminecraft.bitofeverything.init.ModCapabilities;
-import cjminecraft.bitofeverything.init.ModCapabilities.CapabilityWorker;
+import cjminecraft.bitofeverything.util.Utils;
+import cjminecraft.core.energy.CustomForgeEnergyStorage;
+import cjminecraft.core.energy.EnergyUnits;
+import cjminecraft.core.energy.EnergyUtils;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
@@ -18,22 +30,52 @@ public class TileEntityFurnaceGenerator extends TileEntity implements ITickable 
 
 	private ItemStackHandler handler;
 	private Worker worker;
+	private CustomForgeEnergyStorage storage;
+	private ChipTypes type;
 
 	public TileEntityFurnaceGenerator() {
+		this.type = this.world.getBlockState(this.pos).getValue(BlockMachine.TYPE);
 		this.handler = new ItemStackHandler(1);
-		this.worker = new Worker(0, () -> {
+		this.storage = new CustomForgeEnergyStorage(type == ChipTypes.BASIC ? 100000 : 500000, 0, type == ChipTypes.BASIC ? 1000 : 5000);
+		this.worker = new Worker(1, () -> {
 			//Do work
+			if(this.worker.getMaxWork() != 1)
+				if(this.storage.getMaxEnergyStored() - this.storage.getEnergyStored() > (type == ChipTypes.BASIC ? 40 : 60))
+					this.storage.receiveEnergyInternal((type == ChipTypes.BASIC ? 40 : 60), false);
 		}, () -> {
 			//Work done
+			this.worker.setMaxCooldown(1);
+			if(this.handler.getStackInSlot(0).getItem() == Items.BUCKET)
+				return;
+			ItemStack fuel = this.handler.extractItem(0, 1, false);
+			if(fuel != ItemStack.EMPTY)
+				if(this.storage.getMaxEnergyStored() - this.storage.getEnergyStored() > (type == ChipTypes.BASIC ? 40 : 60))
+					this.worker.setMaxCooldown(TileEntityFurnace.getItemBurnTime(fuel));
+			if(fuel.getItem() == Items.LAVA_BUCKET)
+				this.handler.setStackInSlot(0, new ItemStack(Items.BUCKET));
 		});
 	}
 
 	public TileEntityFurnaceGenerator(ChipTypes type) {
+		this.type = type;
 		this.handler = new ItemStackHandler(1);
-		this.worker = new Worker(0, () -> {
+		this.storage = new CustomForgeEnergyStorage(type == ChipTypes.BASIC ? 100000 : 500000, 0, type == ChipTypes.BASIC ? 1000 : 5000);
+		this.worker = new Worker(1, () -> {
 			//Do work
+			if(this.worker.getMaxWork() != 1)
+				if(this.storage.getMaxEnergyStored() - this.storage.getEnergyStored() > (type == ChipTypes.BASIC ? 40 : 60))
+					this.storage.receiveEnergyInternal((type == ChipTypes.BASIC ? 40 : 60), false);
 		}, () -> {
 			//Work done
+			this.worker.setMaxCooldown(1);
+			if(this.handler.getStackInSlot(0).getItem() == Items.BUCKET)
+				return;
+			ItemStack fuel = this.handler.extractItem(0, 1, false);
+			if(fuel != ItemStack.EMPTY)
+				if(this.storage.getMaxEnergyStored() - this.storage.getEnergyStored() > (type == ChipTypes.BASIC ? 40 : 60))
+					this.worker.setMaxCooldown(TileEntityFurnace.getItemBurnTime(fuel));
+			if(fuel.getItem() == Items.LAVA_BUCKET)
+				this.handler.setStackInSlot(0, new ItemStack(Items.BUCKET));
 		});
 	}
 
@@ -41,15 +83,32 @@ public class TileEntityFurnaceGenerator extends TileEntity implements ITickable 
 	public void update() {
 		if (this.world != null) {
 			if (!this.world.isRemote) {
-				
+				int extract = this.storage.getEnergyStored() > (this.type == ChipTypes.BASIC ? 1000 : 5000) ? (this.type == ChipTypes.BASIC ? 1000 : 5000) : this.storage.getEnergyStored();
+				this.storage.extractEnergyInternal((int) EnergyUtils.giveEnergyAllFaces(this.world, this.pos, extract,
+						EnergyUnits.FORGE_ENERGY, false), false);
+				if(this.storage.getMaxEnergyStored() - this.storage.getEnergyStored() > (this.type == ChipTypes.BASIC ? 40 : 60)) {
+					this.worker.doWork();
+					this.markDirty();
+					this.world.setBlockState(this.pos, this.world.getBlockState(this.pos).withProperty(BlockFurnaceGenerator.ACTIVATED, true), 2);
+				} else {
+					this.world.setBlockState(this.pos, this.world.getBlockState(this.pos).withProperty(BlockFurnaceGenerator.ACTIVATED, false), 2);
+				}
+				if(this.worker.getMaxWork() == 1)
+					this.world.setBlockState(this.pos, this.world.getBlockState(this.pos).withProperty(BlockFurnaceGenerator.ACTIVATED, false), 2);
 			}
 		}
+	}
+	
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState) {
+		return oldState.getBlock() != newState.getBlock();
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
 		this.handler.deserializeNBT(nbt.getCompoundTag("Inventory"));
 		this.worker.deserializeNBT(nbt.getCompoundTag("Worker"));
+		this.storage.readFromNBT(nbt);
 		super.readFromNBT(nbt);
 	}
 
@@ -57,12 +116,13 @@ public class TileEntityFurnaceGenerator extends TileEntity implements ITickable 
 	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
 		nbt.setTag("Inventory", this.handler.serializeNBT());
 		nbt.setTag("Worker", this.worker.serializeNBT());
+		this.storage.writeToNBT(nbt);
 		return super.writeToNBT(nbt);
 	}
 
 	@Override
 	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
-		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY || capability == ModCapabilities.CAPABILITY_WORKER)
+		if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY || capability == ModCapabilities.CAPABILITY_WORKER || capability == CapabilityEnergy.ENERGY)
 			return true;
 		return super.hasCapability(capability, facing);
 	}
@@ -73,6 +133,8 @@ public class TileEntityFurnaceGenerator extends TileEntity implements ITickable 
 			return (T) this.handler;
 		if(capability == ModCapabilities.CAPABILITY_WORKER)
 			return (T) this.worker;
+		if(capability == CapabilityEnergy.ENERGY)
+			return (T) this.storage;
 		return super.getCapability(capability, facing);
 	}
 
